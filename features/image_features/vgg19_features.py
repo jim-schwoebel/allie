b88f14c8d0ea2c3737c0b7dfb534a32560a7c774
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2019-06-05 14:52:35 -0400

    Update vgg19_features.py

--- features/image_features/vgg19_features.py
+++ features/image_features/vgg19_features.py
@@ -19,7 +19,7 @@ def vgg19_featurize(file):
     # feature shape = (25088,)
     labels=list()
     for i in range(len(features)):
-    	labels.append('VGG19_feature_%s'%(str(i+1)))
+    	labels.append('vgg19_feature_%s'%(str(i+1)))
     return features, labels 
 
 

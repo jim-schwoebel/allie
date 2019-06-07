7b4278f59620dcd15ea5ca5a6bd4f7dfe223ee48
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2019-06-07 12:27:14 -0400

    Add files via upload

--- datasets/labeling/helpers/balancedelete.py
+++ datasets/labeling/helpers/balancedelete.py
@@ -1,51 +1,55 @@
-import os
-import random
-import shutil
+import os, random, shutil
 
-folder1=input('path to folder1')
-folder2=input('path to folder2')
+## helper functions 
 
-#wavfiles
+def get_wav():
+    # get all .WAV or .MP3 files in folder and count the number of them 
+    listdir=os.listdir()
+    count=0
+    for i in range(len(listdir)):
+        if listdir[i][-4:] in ['.wav', '.mp3']:
+            count = count+1 
+    return count 
 
-os.chdir(folder1)
-listdir1=os.listdir()
-filelist1=list()
-for i in range(len(listdir1)):
-    if listdir1[i][-4:]=='.wav':
-        filelist1.append(listdir1[i])
+def random_remove(remove_num):
+    # remove a number of files to balnace classes.
+    listdir=os.listdir()
+    wavfiles=list()
+    random.shuffle(listdir)
+    for i in range(len(listdir)):
+        if listdir[i][-4:] in ['.wav', '.mp3']:
+            wavfiles.append(listdir[i])
+    for i in range(remove_num):
+        os.remove(wavfiles[i])
+    print('removed %s .wav or .mp3 files'%(remove_num))
 
-os.chdir(folder2)
-listdir2=os.listdir()
-filelist2=list()
-for i in range(len(listdir2)):
-    if listdir2[i][-4:]=='.wav':
-        filelist2.append(listdir2[i])
 
-l1=len(filelist1)
-l2=len(filelist2)
+# now go to main script 
+listdir=os.listdir()
+# find all folders
+folders=list()
+for i in range(len(listdir)):
+    if listdir[i].find('.') < 0:
+        folders.append(listdir[i])
 
-if l1 > l2:
-    dif=l1-l2
-    print('Folder 1 has %s more wave files than folder 2. Deleting files in folder 1 to balance.'%(str(dif)))
-    os.chdir(folder1)
-    removelist=list()
-    for i in range(dif):
-        randnum=random.randint(0,len(filelist1)-1)
-        print('removing %s'%(filelist1[randnum]))
-        os.remove(filelist1[randnum])
-        filelist1.remove(filelist1[randnum])
-    
-elif l2 > l1:
-    dif=l2-l1
-    print('Folder 2 has %s more wave files than folder 1. Deleting files in folder 2 to balance.'%(str(dif)))
-    os.chdir(folder2)
-    removelist=list()
-    for i in range(dif):
-        randnum=random.randint(0,len(filelist2)-1)
-        print('removing %s'%(filelist2[randnum]))
-        os.remove(filelist2[randnum])
-        filelist2.remove(filelist2[randnum])
-else:
-    print('Both folders have the same number of wave files, already. Passing...')
+curdir=os.getcwd()
+counts=list()
+for i in range(len(folders)):
+    os.chdir(curdir)
+    os.chdir(folders[i])
+    count=get_wav()
+    counts.append(count)
+
+# now find minimum
+min_=min(counts)
+
+for i in range(len(folders)):
+    os.chdir(curdir)
+    os.chdir(folders[i])
+    count=get_wav()
+    if count > min_:
+        remove_num=count-min_
+        random_remove(remove_num)
+
 
                     

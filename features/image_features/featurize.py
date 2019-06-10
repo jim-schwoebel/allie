cefbcb50dfbd795d7989b243d7692c7fca326708
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2019-06-10 18:18:01 -0400

    edited schema for sample.

--- features/image_features/featurize.py
+++ features/image_features/featurize.py
@@ -12,14 +12,47 @@ import os, json
 ##				Helper functions.    			##
 ##################################################
 
-def transcribe(transcript):
+def prev_dir(directory):
+	g=directory.split('/')
+	dir_=''
+	for i in range(len(g)):
+		if i != len(g)-1:
+			if i==0:
+				dir_=dir_+g[i]
+			else:
+				dir_=dir_+'/'+g[i]
+	# print(dir_)
+	return dir_
 
-	transcript={'transcript': transcript,
-				'transcript_type': 'tesseract'}
+def transcribe(file, default_image_transcriber):
 
-	return transcript 
+	# 3 types of transcripts = audio, text, and image 
+	transcript_dict={'transcript': transcript,
+					'transcript_type': 'image',
+					'audio_transcriber': default_image_transcriber}
 
-def make_features():
+	return transcript_dict, transcript 
+
+def image_featurize(feature_set, imgfile, cur_dir, haar_dir):
+
+	if feature_set == 'image_features':
+		features, labels=imf.image_featurize(cur_dir, haar_dir, imgfile)
+	elif feature_set == 'vgg16_features':
+		features, labels=v16f.vgg16_featurize(imgfile)
+	elif feature_set == 'inception_features':
+		features, labels=incf.inception_featurize(imgfile)
+	elif feature_set == 'xception_features':
+		features, labels=xf.xception_featurize(imgfile)
+	elif feature_set == 'resnet_features':
+		features, labels=rf.resnet_featurize(imgfile)
+	elif feature_set == 'vgg19_features':
+		features, labels=v16f.vgg19_featurize(imgfile)
+	elif feature_set == 'tesseract_features':
+		transcript, features, labels = tf.tesseract_featurize(imgfile)
+
+	return features, labels 
+
+def make_features(sampletype):
 
 	# only add labels when we have actual labels.
 	features={'audio':dict(),
@@ -28,8 +61,10 @@ def make_features():
 			  'video':dict(),
 			  'csv': dict(),
 			  }
-			  
-	data={'features': features,
+
+	data={'sampletype': sampletype,
+		  'transcripts': [],
+		  'features': features,
 		  'labels': []}
 
 	return data
@@ -46,39 +81,45 @@ os.chdir(foldername)
 cur_dir=os.getcwd()
 listdir=os.listdir() 
 
+# settings directory 
+settingsdir=prev_dir(basedir)
+settings=json.load(open(prev_dir(settingsdir)+'/settings.json'))
+os.chdir(basedir)
+
+image_transcribe=settings['transcribe_image']
+default_image_transcriber=settings['default_image_transcriber']
+feature_set=settings['default_image_features']
+
+#### Can specify a few feature sets here (customizable in settings.json)
 # feature_set='image_features'
 # feature_set='VGG16_features'
 # feature_set='Inception_features'
 # feature_set='Xception_features'
 # feature_set='Resnet50_features'
 # feature_set='VGG19_features'
-feature_set='tesseract_features'
+# feature_set='tesseract_features'
 
 # featurize all files accoridng to librosa featurize
 for i in range(len(listdir)):
-	
+	os.chdir(cur_dir)
 	if listdir[i][-4:] in ['.jpg', '.png']:
 		#try:
 		imgfile=listdir[i]
-			
+		sampletype='image'
+
+		if image_transcribe==True:
+			transcript, features, labels = tf.tesseract_featurize(imgfile)
+			transcript_dict, transcript =transcribe(transcript, default_image_transcriber)
+		
+		# get the features and labels according to specified feature_set 
+		features, labels = image_featurize(feature_set, imgfile, cur_dir, haar_dir)
+
 		# I think it's okay to assume audio less than a minute here...
 		if listdir[i][0:-4]+'.json' not in listdir:
 			# make new .JSON if it is not there with base array schema.
-			basearray=make_features()
+			basearray=make_features(sampletype)
 			image_features=basearray['features']['image']
-
-			# features, labels=imf.image_featurize(cur_dir, haar_dir, imgfile)
-			# features, labels=v16f.vgg16_featurize(imgfile)
-			# features, labels=incf.inception_featurize(imgfile)
-			# features, labels=xf.xception_featurize(imgfile)
-			# features, labels=rf.resnet_featurize(imgfile)
-			# features, labels=v16f.vgg16_featurize(imgfile)
-
-			# specialty feature script for pytesseract 
-			transcript, features, labels = tf.tesseract_featurize(imgfile)
-			transcript=transcribe(transcript)
-			basearray['transcript']=transcript
-
+			basearray['transcript']=[transcript_dict]
 			print(features)
 
 			try:
@@ -97,20 +138,10 @@ for i in range(len(listdir)):
 		elif listdir[i][0:-4]+'.json' in listdir:
 			# overwrite existing .JSON if it is there.
 			basearray=json.load(open(listdir[i][0:-4]+'.json'))
-			
-			# features, labels=imf.image_featurize(cur_dir, haar_dir, imgfile)
-			# features, labels=v16f.vgg16_featurize(imgfile)
-			# features, labels=incf.inception_featurize(imgfile)
-			# features, labels=xf.xception_featurize(imgfile)
-			# features, labels=rf.resnet_featurize(imgfile)
-			# features, labels=v16f.vgg16_featurize(imgfile)
-
-			# specialty feature script for pytesseract 
-			transcript, features, labels = tf.tesseract_featurize(imgfile)
-			transcript=transcribe(transcript)
-			basearray['transcript']=transcript
-
-			# print(features)
+			transcript_list=basearray['transcript']
+			transcript_list.append(transcript_dict)
+			basearray['transcript']=transcript_list
+			print(features)
 
 			try:
 				data={'features':features.tolist(),

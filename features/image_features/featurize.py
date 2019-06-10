64c54daba99555fe284875cfb1ad076458c4bc8b
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2019-06-10 19:31:22 -0400

    Add files via upload

--- features/image_features/featurize.py
+++ features/image_features/featurize.py
@@ -1,17 +1,35 @@
-import vgg16_features as v16f
-import image_features as imf 
-import inception_features as incf 
-import xception_features as xf 
-import resnet_features as rf 
-import vgg19_features as v19f
-import tesseract_features as tf
-import helpers.audio_plot as ap 
-import os, json
+'''
+Import all the featurization scripts and allow the user to customize what embedding that
+they would like to use for modeling purposes.
+
+AudioSet is the only embedding that is a little bit wierd, as it is normalized to the length
+of each audio file. There are many ways around this issue (such as normalizing to the length 
+of each second), however, I included all the original embeddings here in case the time series
+information is useful to you.
+'''
+##################################################
+##				Import statements   			##
+##################################################
+
+# scripts 
+import nltk_features as nf
+import spacy_features as sf
+import glove_features as gf 
+import fast_features as ff
+import helpers.transcribe as ts
+
+# other import stuff 
+import json, os, sys
+from gensim.scripts.glove2word2vec import glove2word2vec
+from gensim.models import KeyedVectors
+from gensim.models import Word2Vec
+from gensim.models.fasttext import FastText
+import os, wget, zipfile 
+import shutil
 
 ##################################################
 ##				Helper functions.    			##
 ##################################################
-
 def prev_dir(directory):
 	g=directory.split('/')
 	dir_=''
@@ -23,34 +41,6 @@ def prev_dir(directory):
 				dir_=dir_+'/'+g[i]
 	# print(dir_)
 	return dir_
-
-def transcribe(file, default_image_transcriber):
-
-	# 3 types of transcripts = audio, text, and image 
-	transcript_dict={'transcript': transcript,
-					'transcript_type': 'image',
-					'audio_transcriber': default_image_transcriber}
-
-	return transcript_dict, transcript 
-
-def image_featurize(feature_set, imgfile, cur_dir, haar_dir):
-
-	if feature_set == 'image_features':
-		features, labels=imf.image_featurize(cur_dir, haar_dir, imgfile)
-	elif feature_set == 'vgg16_features':
-		features, labels=v16f.vgg16_featurize(imgfile)
-	elif feature_set == 'inception_features':
-		features, labels=incf.inception_featurize(imgfile)
-	elif feature_set == 'xception_features':
-		features, labels=xf.xception_featurize(imgfile)
-	elif feature_set == 'resnet_features':
-		features, labels=rf.resnet_featurize(imgfile)
-	elif feature_set == 'vgg19_features':
-		features, labels=v16f.vgg19_featurize(imgfile)
-	elif feature_set == 'tesseract_features':
-		transcript, features, labels = tf.tesseract_featurize(imgfile)
-
-	return features, labels 
 
 def make_features(sampletype):
 
@@ -69,58 +59,134 @@ def make_features(sampletype):
 
 	return data
 
+def transcribe(transcript):
+
+	# 3 types of transcripts = audio, text, and image 
+	transcript_dict={'transcript': transcript,
+					'transcript_type': 'text',
+					'text_transcriber': 'raw text'}
+
+	return transcript_dict, transcript 
+
+def text_featurize(feature_set, transcript, glovemodel, w2vmodel, fastmodel):
+
+	if feature_set == 'nltk_features':
+		features, labels = nf.nltk_featurize(transcript)
+	elif feature_set == 'spacy_features':
+		features, labels = sf.spacy_featurize(transcript)
+	elif feature_set == 'glove_features':
+		features, labels=gf.glove_featurize(transcript, glovemodel)
+	elif feature_set == 'w2v_features':
+		features, labels=w2v.w2v_featurize(transcript, w2vmodel)
+	elif feature_set == 'fast_features':
+		features, labels=ff.fast_featurize(transcript, fastmodel)
+
+	return features, labels 
+
+# type in folder before downloading and loading large files.
+foldername=input('what is the name of the folder?')
+
 ##################################################
 ##				   Main script  		    	##
 ##################################################
 
 # directory=sys.argv[1]
 basedir=os.getcwd()
-haar_dir=basedir+'/helpers/haarcascades'
-foldername=input('what is the name of the folder?')
 os.chdir(foldername)
-cur_dir=os.getcwd()
 listdir=os.listdir() 
+cur_dir=os.getcwd()
 
-# settings directory 
+# directory=sys.argv[1]
 settingsdir=prev_dir(basedir)
-settings=json.load(open(prev_dir(settingsdir)+'/settings.json'))
+settingsdir=prev_dir(settingsdir)
+settings=json.load(open(settingsdir+'/settings.json'))
 os.chdir(basedir)
 
-image_transcribe=settings['transcribe_image']
-default_image_transcriber=settings['default_image_transcriber']
-feature_set=settings['default_image_features']
+feature_set=settings['default_text_features']
 
-#### Can specify a few feature sets here (customizable in settings.json)
-# feature_set='image_features'
-# feature_set='VGG16_features'
-# feature_set='Inception_features'
-# feature_set='Xception_features'
-# feature_set='Resnet50_features'
-# feature_set='VGG19_features'
-# feature_set='tesseract_features'
+# can specify many types of features...
+if feature_set in ['nltk_features', 'spacy_features']:
+	# save memory by not loading any models that are not necessary.
+	glovemodel=[]
+	w2vmodel=[]
+	fastmodel=[]
+
+else:
+	##################################################
+	##				Load ML models					##
+	##################################################
+
+	# load GloVE model
+	if feature_set == 'glove_features':
+		if 'glove.6B' not in os.listdir(os.getcwd()+'/helpers'):
+			curdir=os.getcwd()
+			print('downloading GloVe model...')
+			wget.download("http://neurolex.co/uploads/glove.6B.zip", "./helpers/glove.6B.zip")
+			print('extracting GloVe model')
+			zip_ref = zipfile.ZipFile(os.getcwd()+'/helpers/glove.6B.zip', 'r')
+			zip_ref.extractall(os.getcwd()+'/helpers/glove.6B')
+			zip_ref.close()
+			os.chdir(os.getcwd()+'/helpers/glove.6B')
+			glove_input_file = 'glove.6B.100d.txt'
+			word2vec_output_file = 'glove.6B.100d.txt.word2vec'
+			glove2word2vec(glove_input_file, word2vec_output_file)
+			os.chdir(curdir)
+
+		glovemodelname = 'glove.6B.100d.txt.word2vec'
+		print('-----------------')
+		print('loading GloVe model...')
+		glovemodel = KeyedVectors.load_word2vec_format(os.getcwd()+'/helpers/glove.6B/'+glovemodelname, binary=False)
+		print('loaded GloVe model...')
+		w2vmodel=[]
+		fastmodel=[]
+	# load Google W2V model
+	elif feature_set == 'w2v_features':
+		if 'GoogleNews-vectors-negative300.bin' not in os.listdir(os.getcwd()+'/helpers'):
+			print('downloading Google W2V model...')
+			wget.download("http://neurolex.co/uploads/GoogleNews-vectors-negative300.bin", "./helpers/GoogleNews-vectors-negative300.bin")
+
+		w2vmodelname = 'GoogleNews-vectors-negative300.bin'
+		print('-----------------')
+		print('loading Google W2V model...')
+		w2vmodel = KeyedVectors.load_word2vec_format(os.getcwd()+'/helpers/'+w2vmodelname, binary=True)
+		print('loaded Google W2V model...')
+		glovemodel=[]
+		fastmodel=[]
+
+	# load facebook FastText model
+	elif feature_set == 'fast_features':
+		if 'wiki-news-300d-1M' not in os.listdir(os.getcwd()+'/helpers'):
+			print('downloading Facebook FastText model...')
+			wget.download("https://dl.fbaipublicfiles.com/fasttext/vectors-english/wiki-news-300d-1M.vec.zip", "./helpers/wiki-news-300d-1M.vec.zip")
+			zip_ref = zipfile.ZipFile(os.getcwd()+'/helpers/wiki-news-300d-1M.vec.zip', 'r')
+			zip_ref.extractall(os.getcwd()+'/helpers/wiki-news-300d-1M')
+			zip_ref.close()
+
+		print('-----------------')
+		print('loading Facebook FastText model...')
+		# Loading fasttext model 
+		fastmodel = KeyedVectors.load_word2vec_format(os.getcwd()+'/helpers/wiki-news-300d-1M/wiki-news-300d-1M.vec')
+		print('loaded Facebook FastText model...')
+		glovemodel=[]
+		w2vmodel=[]
 
 # featurize all files accoridng to librosa featurize
 for i in range(len(listdir)):
-	os.chdir(cur_dir)
-	if listdir[i][-4:] in ['.jpg', '.png']:
+	if listdir[i][-4:] in ['.txt']:
 		#try:
-		imgfile=listdir[i]
-		sampletype='image'
-
-		if image_transcribe==True:
-			transcript, features, labels = tf.tesseract_featurize(imgfile)
-			transcript_dict, transcript =transcribe(transcript, default_image_transcriber)
-		
-		# get the features and labels according to specified feature_set 
-		features, labels = image_featurize(feature_set, imgfile, cur_dir, haar_dir)
+		sampletype='text'
+		os.chdir(cur_dir)
+		transcript=open(listdir[i]).read()
+		transcript_dict, transcript = transcribe(transcript)
+		features, labels = text_featurize(feature_set, transcript, glovemodel, w2vmodel, fastmodel)
+		print(features)
 
 		# I think it's okay to assume audio less than a minute here...
 		if listdir[i][0:-4]+'.json' not in listdir:
 			# make new .JSON if it is not there with base array schema.
 			basearray=make_features(sampletype)
-			image_features=basearray['features']['image']
-			basearray['transcript']=[transcript_dict]
-			print(features)
+			text_features=basearray['features']['text']
+			basearray['transcripts']=[transcript_dict]
 
 			try:
 				data={'features':features.tolist(),
@@ -129,19 +195,19 @@ for i in range(len(listdir)):
 				data={'features':features,
 					  'labels': labels}
 
-			image_features[feature_set]=data
-			basearray['features']['image']=image_features
+			text_features[feature_set]=data
+			basearray['features']['text']=text_features
 			basearray['labels']=[foldername]
 			jsonfile=open(listdir[i][0:-4]+'.json','w')
 			json.dump(basearray, jsonfile)
 			jsonfile.close()
+
 		elif listdir[i][0:-4]+'.json' in listdir:
 			# overwrite existing .JSON if it is there.
 			basearray=json.load(open(listdir[i][0:-4]+'.json'))
-			transcript_list=basearray['transcript']
+			transcript_list=basearray['transcripts']
 			transcript_list.append(transcript_dict)
-			basearray['transcript']=transcript_list
-			print(features)
+			basearray['transcripts']=transcript_list
 
 			try:
 				data={'features':features.tolist(),
@@ -150,7 +216,7 @@ for i in range(len(listdir)):
 				data={'features':features,
 					  'labels': labels}
 
-			basearray['features']['image'][feature_set]=data
+			basearray['features']['text'][feature_set]=data
 			basearray['labels']=[foldername]
 			jsonfile=open(listdir[i][0:-4]+'.json','w')
 			json.dump(basearray, jsonfile)
@@ -158,3 +224,8 @@ for i in range(len(listdir)):
 
 		#except:
 			#print('error')
+
+
+
+
+

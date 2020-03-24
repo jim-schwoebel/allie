d23273ede067376a768b306b854c8b4e535a8ac6
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-03-24 11:19:17 -0400

    Add files via upload

---
+++ features/video_features/helpers/tesseract_features.py
@@ -0,0 +1,28 @@
+import os, sys, time
+from PIL import Image
+import pytesseract 
+
+def prev_dir(directory):
+	g=directory.split('/')
+	# print(g)
+	lastdir=g[len(g)-1]
+	i1=directory.find(lastdir)
+	directory=directory[0:i1]
+	return directory
+	
+directory=os.getcwd()
+prevdir=prev_dir(directory)
+sys.path.append(prevdir+'/text_features')
+import nltk_features as nf 
+os.chdir(directory)
+
+def transcribe_image(imgfile):
+	transcript=pytesseract.image_to_string(Image.open(imgfile))
+	return transcript 
+
+def tesseract_featurize(imgfile):
+	# can stitch across an entire length of video frames too 
+	transcript=transcribe_image(imgfile)
+	features, labels = nf.nltk_featurize(transcript)
+	
+	return transcript, features, labels 

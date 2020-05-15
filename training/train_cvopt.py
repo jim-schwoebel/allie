2a6ed5cb0803d7dc9b8701855e2f5b53f99f60cd
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-05-15 12:40:45 -0400

    updated script

--- training/train_cvopt.py
+++ training/train_cvopt.py
@@ -7,14 +7,15 @@ from cvopt.model_selection import SimpleoptCV
 from cvopt.search_setting import search_category, search_numeric
 
 
-def train_cvopt(alldata, labels, mtype, jsonfile, problemtype, default_features, settings):
+def train_cvopt(X_train,X_test,y_train,y_test,mtype,common_name_model,problemtype,classes,default_featurenames,transform_model,settings,model_session):
 	# downloading library
 	os.system('pip3 install cvopt==0.4.2')
+	os.system('pip3 install scikit-learn==0.22')
+	os.system('pip3 install bokeh==1.4.0')
 
-	# training and testing sets
-	Xtrain, Xtest, ytrain, ytest = train_test_split(alldata, labels, train_size=0.750, test_size=0.250)
-	modelname=jsonfile[0:-5]+'_cvopt_'+str(default_features).replace("'",'').replace('"','')
-	model_name=modelname+'.pickle'
+	# create model name
+	model_name=common_name_model
+	files=list()
 
 	param_distributions = {
 		"penalty": search_category(['none', 'l2']),
@@ -39,9 +40,8 @@ def train_cvopt(alldata, labels, mtype, jsonfile, problemtype, default_features,
 						 backend=bk,                     # hyperopt,bayesopt, gaopt or randomopt.
 						 )
 
-		opt.fit(Xtrain, ytrain, validation_data=(Xtest, ytest))
-		ytest_pred = opt.predict(Xtest)
-
+		opt.fit(X_train, y_train, validation_data=(X_test, y_test))
+		ytest_pred = opt.predict(X_test)
 
 		from cvopt.utils import extract_params
 		target_index = pd.DataFrame(opt.cv_results_)[pd.DataFrame(opt.cv_results_)["mean_test_score"] == opt.best_score_]["index"].values[0]
@@ -51,39 +51,29 @@ def train_cvopt(alldata, labels, mtype, jsonfile, problemtype, default_features,
 																				target_index=target_index)
 																   
 		estimator.set_params(**estimator_params)         # Set estimator parameters
-		Xtrain_selected = Xtrain[:, feature_select_flag] # Extract selected feature columns
+		X_train_selected = X_train[:, feature_select_flag] # Extract selected feature columns
 
 		print(estimator)
-		print("Train features shape:", Xtrain.shape)
-		print("Train selected features shape:",Xtrain_selected.shape)
+		print("Train features shape:", X_train.shape)
+		print("Train selected features shape:",X_train_selected.shape)
 
 		from cvopt.utils import mk_metafeature
-		Xtrain_meta, Xtest_meta = mk_metafeature(Xtrain, ytrain, 
+		X_train_meta, X_test_meta = mk_metafeature(X_train, y_train, 
 												 logdir="./search_usage", 
 												 model_id=bk, 
 												 target_index=target_index, 
 												 cv=cv, 
-												 validation_data=(Xtest, ytest), 
+												 validation_data=(X_test, y_test), 
 												 estimator_method="predict_proba")
 
-		print("Train features shape:", Xtrain.shape)
-		print("Train meta features shape:", Xtrain_meta.shape)
-		print("Test features shape:", Xtest.shape)
-		print("Test meta features shape:",  Xtest_meta.shape)
+		print("Train features shape:", X_train.shape)
+		print("Train meta features shape:", X_train_meta.shape)
+		print("Test features shape:", X_test.shape)
+		print("Test meta features shape:",  X_test_meta.shape)
 
-	cur_dir2=os.getcwd()
-	os.rename('search_usage', modelname)
 
-	try:
-		os.chdir(problemtype+'_models')
-	except:
-		os.mkdir(problemtype+'_models')
-		os.chdir(problemtype+'_models')
-
-	# now move all the files over to proper model directory 
-	shutil.copytree(cur_dir2+'/'+modelname, os.getcwd()+'/'+modelname)
-	shutil.rmtree(cur_dir2+'/'+modelname)
-
+	os.rename('search_usage', model_name)
+	files.append(model_name)
 	model_dir=os.getcwd()
 
-	return model_name, model_dir
+	return model_name, model_dir, files

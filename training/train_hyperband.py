458cb3a1722568b9c05c12a2834df513a6774eed
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-05-15 22:32:11 -0400

    Add files via upload

--- training/train_hyperband.py
+++ training/train_hyperband.py
@@ -1,24 +1,32 @@
-import os, sys, shutil, pickle, json
+import os, pickle
 from scipy.stats import randint as sp_randint
-from sklearn.datasets import load_digits
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.model_selection import train_test_split
 
-def train_hyperband(alldata, labels, mtype, jsonfile, problemtype, default_features, settings):
+def prev_dir(directory):
+	g=directory.split('/')
+	dir_=''
+	for i in range(len(g)):
+		if i != len(g)-1:
+			if i==0:
+				dir_=dir_+g[i]
+			else:
+				dir_=dir_+'/'+g[i]
+	# print(dir_)
+	return dir_
+
+def train_hyperband(X_train,X_test,y_train,y_test,mtype,common_name_model,problemtype,classes,default_featurenames,transform_model,settings,model_session):
 	# install
-	os.system('pip3 install scikit-hyperband==0.0.1')
+	curdir=os.getcwd()
+	os.chdir(prev_dir(os.getcwd())+'/training/helpers/hyperband')
+	os.system('python3 setup.py install')
 	from hyperband import HyperbandSearchCV
+	os.chdir(curdir)
 	
 	# training and testing sets
-	X, X_test, y, y_test = train_test_split(alldata, labels, train_size=0.750, test_size=0.250)
+	files=list()
+	model_name=common_name_model+'.pickle'
 
 	if mtype in ['classification', 'c']:
-
-		modelname=jsonfile[0:-5]+'_hyperband_classification_'+str(default_features).replace("'",'').replace('"','')
-		model_name=modelname+'.pickle'
-		model_transformer=modelname+'_transformer.pickle'
-		jsonfilename=modelname+'.json'
 
 		model = RandomForestClassifier()
 		param_dist = {
@@ -30,58 +38,21 @@ def train_hyperband(alldata, labels, mtype, jsonfile, problemtype, default_featu
 		    'criterion': ['gini', 'entropy']
 		}
 
-		transformer=LabelBinarizer().fit(y)
-		y_new = transformer.transform(y)
-
 		search = HyperbandSearchCV(model, param_dist, 
 		                           resource_param='n_estimators',
 		                           scoring='roc_auc')
-		search.fit(X, y_new)
+		search.fit(X_train, y_train)
 		params=search.best_params_
 		print('-----')
 		print('best params: ')
 		print(params)
 		print('------')
-		accuracy=search.score(X_test, transformer.transform(y_test))
+		accuracy=search.score(X_test, y_test)
 
 		# SAVE ML MODEL
 		modelfile=open(model_name,'wb')
 		pickle.dump(search, modelfile)
 		modelfile.close()
-
-		# SAVE TRANSFORMER 
-		modelfile=open(model_transformer, 'wb')
-		pickle.dump(transformer, modelfile)
-		modelfile.close()
-
-		# SAVE JSON FILE 
-		print('saving .JSON file (%s)'%(jsonfilename))
-		jsonfile=open(jsonfilename,'w')
-
-		data={'sample type': problemtype,
-			'feature_set':default_features,
-			'model name':model_name,
-			'accuracy':accuracy,
-			'model type':'hyperband_randomforest_classification',
-			'settings': settings,
-			'transformer': model_transformer,
-		}
-
-		json.dump(data,jsonfile)
-		jsonfile.close()
-
-		# move model to proper directory
-		cur_dir2=os.getcwd()
-		try:
-			os.chdir(problemtype+'_models')
-		except:
-			os.mkdir(problemtype+'_models')
-			os.chdir(problemtype+'_models')
-
-		# now move all the files over to proper model directory 
-		shutil.move(cur_dir2+'/'+jsonfilename, os.getcwd()+'/'+jsonfilename)
-		shutil.move(cur_dir2+'/'+model_name, os.getcwd()+'/'+model_name)
-		shutil.move(cur_dir2+'/'+model_transformer, os.getcwd()+'/'+model_transformer)
 
 	elif mtype in ['regression', 'r']:
 
@@ -89,5 +60,6 @@ def train_hyperband(alldata, labels, mtype, jsonfile, problemtype, default_featu
 		model_name=''
 
 	model_dir=os.getcwd()
+	files.append(model_name)
 
-	return model_name, model_dir
+	return model_name, model_dir, files

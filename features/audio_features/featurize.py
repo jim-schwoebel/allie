33eb76d09e48cd2b3f0afca51ce77ede4718cb22
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-05-15 21:11:50 -0400

    Add files via upload

--- features/audio_features/featurize.py
+++ features/audio_features/featurize.py
@@ -8,9 +8,10 @@ of each second), however, I included all the original embeddings here in case th
 information is useful to you.
 '''
 
-import json, os, sys
+import json, os, sys, time
 import numpy as np 
 import helpers.transcribe as ts
+import speech_recognition as sr
 from tqdm import tqdm
 
 def prev_dir(directory):
@@ -39,21 +40,178 @@ os.chdir(directory)
 ##	    		Helper functions      		  ##
 ################################################
 
-def transcribe(file, default_audio_transcriber):
+def transcribe(file, default_audio_transcriber, settingsdir):
 	# create all transcription methods here
 	print('%s transcribing: %s'%(default_audio_transcriber, file))
-	try:
-		if file[-4:]=='.wav':
-			transcript=ts.transcribe_sphinx(file)
-		elif file[-4] == '.mp3':
-			os.system('ffmpeg -i %s %s'%(file, file[0:-4]+'.wav'))
-			transcript=ts.transcribe_sphinx(file)
-			os.remove(file[-4:]+'.wav')
-		else:
-			transcript=file
-	except:
+
+	# use the audio file as the audio source
+	r = sr.Recognizer()
+	transcript_engine = default_audio_transcriber
+
+	with sr.AudioFile(file) as source:
+		audio = r.record(source)  # read the entire audio file
+
+	if transcript_engine == 'pocketsphinx':
+
+		# recognize speech using Sphinx
+		try:
+			transcript= r.recognize_sphinx(audio)
+		except sr.UnknownValueError:
+			transcript=''
+		except sr.RequestError as e:
+			transcript=''
+
+	elif transcript_engine == 'deepspeech_nodict':
+
+		curdir=os.getcwd()
+		os.chdir(settingsdir+'/features/audio_features/helpers')
+		listdir=os.listdir()
+		deepspeech_dir=os.getcwd()
+
+		# download models if not in helper directory
+		if 'deepspeech-0.7.0-models.pbmm' not in listdir:
+			os.system('wget https://github.com/mozilla/DeepSpeech/releases/download/v0.7.0/deepspeech-0.7.0-models.pbmm')
+
+		# initialize filenames
+		textfile=file[0:-4]+'.txt'
+		newaudio=file[0:-4]+'_newaudio.wav'
+		
+		if deepspeech_dir.endswith('/'):
+			deepspeech_dir=deepspeech_dir[0:-1]
+
+		# go back to main directory
+		os.chdir(curdir)
+
+		# convert audio file to 16000 Hz mono audio 
+		os.system('ffmpeg -i "%s" -acodec pcm_s16le -ac 1 -ar 16000 "%s" -y'%(file, newaudio))
+		command='deepspeech --model %s/deepspeech-0.7.0-models.pbmm --audio "%s" >> "%s"'%(deepspeech_dir, newaudio, textfile)
+		print(command)
+		os.system(command)
+
+		# get transcript
+		transcript=open(textfile).read().replace('\n','')
+
+		# remove temporary files
+		os.remove(textfile)
+		os.remove(newaudio)
+
+	elif transcript_engine == 'deepspeech_dict':
+
+		curdir=os.getcwd()
+		os.chdir(settingsdir+'/features/audio_features/helpers')
+		listdir=os.listdir()
+		deepspeech_dir=os.getcwd()
+
+		# download models if not in helper directory
+		if 'deepspeech-0.7.0-models.pbmm' not in listdir:
+			os.system('wget https://github.com/mozilla/DeepSpeech/releases/download/v0.7.0/deepspeech-0.7.0-models.pbmm')
+		if 'deepspeech-0.7.0-models.scorer' not in listdir:
+			os.system('wget https://github.com/mozilla/DeepSpeech/releases/download/v0.7.0/deepspeech-0.7.0-models.scorer')
+
+		# initialize filenames
+		textfile=file[0:-4]+'.txt'
+		newaudio=file[0:-4]+'_newaudio.wav'
+		
+		if deepspeech_dir.endswith('/'):
+			deepspeech_dir=deepspeech_dir[0:-1]
+
+		# go back to main directory
+		os.chdir(curdir)
+
+		# convert audio file to 16000 Hz mono audio 
+		os.system('ffmpeg -i "%s" -acodec pcm_s16le -ac 1 -ar 16000 "%s" -y'%(file, newaudio))
+		command='deepspeech --model %s/deepspeech-0.7.0-models.pbmm --scorer %s/deepspeech-0.7.0-models.scorer --audio "%s" >> "%s"'%(deepspeech_dir, deepspeech_dir, newaudio, textfile)
+		print(command)
+		os.system(command)
+
+		# get transcript
+		transcript=open(textfile).read().replace('\n','')
+
+		# remove temporary files
+		os.remove(textfile)
+		os.remove(newaudio)
+
+	elif transcript_engine == 'google':
+
+		# recognize speech using Google Speech Recognition
+			# for testing purposes, we're just using the default API key
+			# to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+			# instead of `r.recognize_google(audio)`
+
+		# recognize speech using Google Cloud Speech
+		GOOGLE_CLOUD_SPEECH_CREDENTIALS = os.environ['GOOGLE_APPLICATION_CREDENTIALS']
+		print(GOOGLE_CLOUD_SPEECH_CREDENTIALS)
+
+		try:
+			transcript=r.recognize_google_cloud(audio, credentials_json=open(GOOGLE_CLOUD_SPEECH_CREDENTIALS).read())
+		except sr.UnknownValueError:
+			transcript=''
+		except sr.RequestError as e:
+			transcript=''
+
+	elif transcript_engine == 'wit':
+
+		# recognize speech using Wit.ai
+		WIT_AI_KEY = os.environ['WIT_AI_KEY']
+
+		try:
+			transcript=r.recognize_wit(audio, key=WIT_AI_KEY)
+		except sr.UnknownValueError:
+			transcript=''
+		except sr.RequestError as e:
+			transcript=''
+
+	elif transcript_engine == 'azure':
+
+		# recognize speech using Microsoft Azure Speech
+		AZURE_SPEECH_KEY = os.environ['AZURE_SPEECH_KEY']
+		print(AZURE_SPEECH_KEY)
+		try:
+			transcript=r.recognize_azure(audio, key=AZURE_SPEECH_KEY)
+		except sr.UnknownValueError:
+			transcript=''
+		except sr.RequestError as e:
+			transcript=''
+
+	elif transcript_engine == 'bing':
+		# recognize speech using Microsoft Bing Voice Recognition
+		BING_KEY = os.environ['BING_KEY']
+		try:
+			transcript=r.recognize_bing(audio, key=BING_KEY)
+		except sr.UnknownValueError:
+			transcript=''
+		except sr.RequestError as e:
+			transcript=''
+
+	elif transcript_engine == 'houndify':
+		# recognize speech using Houndify
+		HOUNDIFY_CLIENT_ID = os.environ['HOUNDIFY_CLIENT_ID']  
+		HOUNDIFY_CLIENT_KEY = os.environ['HOUNDIFY_CLIENT_KEY']  
+		try:
+			transcript=r.recognize_houndify(audio, client_id=HOUNDIFY_CLIENT_ID, client_key=HOUNDIFY_CLIENT_KEY)
+		except sr.UnknownValueError:
+			transcript=''
+		except sr.RequestError as e:
+			transcript=''
+
+	elif transcript_engine == 'ibm':
+		# recognize speech using IBM Speech to Text
+		IBM_USERNAME = os.environ['IBM_USERNAME']
+		IBM_PASSWORD = os.environ['IBM_PASSWORD']
+
+		try:
+			transcript=r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD)
+		except sr.UnknownValueError:
+			transcript=''
+		except sr.RequestError as e:
+			transcript=''
+
+	else:
+		print('no transcription engine specified')
 		transcript=''
 
+	# show transcript
+	print(transcript_engine.upper())
 	print('--> '+ transcript)
 
 	return transcript 
@@ -268,15 +426,13 @@ for i in tqdm(range(len(listdir)), desc=labelname):
 				basearray=make_features(sampletype)
 
 				# get the first audio transcriber and loop through transcript list
-				for j in range(len(default_audio_transcribers)):
-					default_audio_transcriber=default_audio_transcribers[j]
-					if audio_transcribe==True:
-						transcript = transcribe(filename, default_audio_transcriber)
+				if audio_transcribe==True:
+					for j in range(len(default_audio_transcribers)):
+						default_audio_transcriber=default_audio_transcribers[j]
+						transcript = transcribe(filename, default_audio_transcriber, settingsdir)
 						transcript_list=basearray['transcripts']
 						transcript_list['audio'][default_audio_transcriber]=transcript 
 						basearray['transcripts']=transcript_list
-					else:
-						transcript=''
 
 				# featurize the audio file 
 				for j in range(len(feature_sets)):
@@ -306,19 +462,20 @@ for i in tqdm(range(len(listdir)), desc=labelname):
 				transcript_list=basearray['transcripts']
 
 				# only transcribe if you need to (checks within schema)
-				for j in range(len(default_audio_transcribers)):
+				if audio_transcribe==True:
+					for j in range(len(default_audio_transcribers)):
 
-					# get the first audio transcriber and loop through transcript list
-					default_audio_transcriber=default_audio_transcribers[j]
+						# get the first audio transcriber and loop through transcript list
+						default_audio_transcriber=default_audio_transcribers[j]
 
-					if audio_transcribe==True and default_audio_transcriber not in list(transcript_list['audio']):
-						transcript = transcribe(filename, default_audio_transcriber)
-						transcript_list['audio'][default_audio_transcriber]=transcript 
-						basearray['transcripts']=transcript_list
-					elif audio_transcribe==True and default_audio_transcriber in list(transcript_list['audio']):
-						transcript = transcript_list['audio'][default_audio_transcriber]
-					else:
-						transcript=''
+						if audio_transcribe==True and default_audio_transcriber not in list(transcript_list['audio']):
+							transcript = transcribe(filename, default_audio_transcriber, settingsdir)
+							transcript_list['audio'][default_audio_transcriber]=transcript 
+							basearray['transcripts']=transcript_list
+						elif audio_transcribe==True and default_audio_transcriber in list(transcript_list['audio']):
+							transcript = transcript_list['audio'][default_audio_transcriber]
+						else:
+							transcript=''
 					
 				# only re-featurize if necessary (checks if relevant feature embedding exists)
 				for j in range(len(feature_sets)):

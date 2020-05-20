e81dc05b3e3ce7ddec5d8826c50ab3661288b9ee
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-05-20 17:14:40 -0400

    fixing bug in csv transcription

--- features/csv_features/featurize.py
+++ features/csv_features/featurize.py
@@ -50,8 +50,11 @@ def make_features(sampletype):
 
 	return data
 
-def transcribe_csv(csv_file):
-	transcript=open(csv_file).read()
+def transcribe_csv(csv_file, csv_transcriber):
+	if csv_transcriber=='raw text':
+		transcript=open(csv_file).read()
+	else:
+		transcript=''
 	return transcript 
 
 def csv_featurize(feature_set, csvfile, cur_dir):
@@ -112,84 +115,89 @@ for i in tqdm(range(len(listdir)), desc=labelname):
 
 	# make audio file into spectrogram and analyze those images if audio file
 	if listdir[i][-4:] in ['.csv']:
-		#try:
-		csv_file=listdir[i]
-		sampletype='csv'
-		# I think it's okay to assume audio less than a minute here...
-		if listdir[i][0:-4]+'.json' not in listdir:
-			# make new .JSON if it is not there with base array schema.
-			basearray=make_features(sampletype)
+		try:
+			csv_file=listdir[i]
+			sampletype='csv'
+			# I think it's okay to assume audio less than a minute here...
+			if listdir[i][0:-4]+'.json' not in listdir:
+				# make new .JSON if it is not there with base array schema.
+				basearray=make_features(sampletype)
 
-			# get the csv transcript  
-			if csv_transcribe==True:
-				transcript = transcribe_csv(csv_file)
-				transcript_list=basearray['transcripts']
-				transcript_list['csv'][default_csv_transcriber]=transcript 
-				basearray['transcripts']=transcript_list
+				# get the csv transcript  
+				if csv_transcribe==True:
+					for j in range(len(default_csv_transcriber)):
+						csv_transcriber=default_csv_transcriber[j]
+						transcript = transcribe_csv(csv_file, csv_transcriber)
+						transcript_list=basearray['transcripts']
+						transcript_list['csv'][csv_transcriber]=transcript 
+						basearray['transcripts']=transcript_list
 
-			# featurize the csv file 
-			for j in range(len(feature_sets)):
-				feature_set=feature_sets[j]
-				features, labels = csv_featurize(feature_set, csv_file, cur_dir)
-				try:
-					data={'features':features.tolist(),
-						  'labels': labels}
-				except:
-					data={'features':features,
-						  'labels': labels}
-
-				print(features)
-				csv_features=basearray['features']['csv']
-				csv_features[feature_set]=data
-				basearray['features']['csv']=csv_features
-
-			basearray['labels']=[labelname]
-
-			# write to .JSON 
-			jsonfile=open(listdir[i][0:-4]+'.json','w')
-			json.dump(basearray, jsonfile)
-			jsonfile.close()
-
-		elif listdir[i][0:-4]+'.json' in listdir:
-			# load the .JSON file if it is there 
-			basearray=json.load(open(listdir[i][0:-4]+'.json'))
-			transcript_list=basearray['transcripts']
-
-			# only transcribe if you need to (checks within schema)
-			if csv_transcribe==True and default_csv_transcriber not in list(transcript_list['csv']):
-				transcript = transcribe_csv(csv_file)
-				transcript_list['csv'][default_csv_transcriber]=transcript 
-				basearray['transcripts']=transcript_list
-			else:
-				transcript = transcript_list['csv'][default_csv_transcriber]
-
-			# only re-featurize if necessary (checks if relevant feature embedding exists)
-			for j in range(len(feature_sets)):
-				feature_set=feature_sets[j]
-				if feature_set not in list(basearray['features']['csv']):
-					features, labels = cf.csv_featurize(csv_file, cur_dir)
-					print(features)
-
+				# featurize the csv file 
+				for j in range(len(feature_sets)):
+					feature_set=feature_sets[j]
+					features, labels = csv_featurize(feature_set, csv_file, cur_dir)
 					try:
 						data={'features':features.tolist(),
 							  'labels': labels}
 					except:
 						data={'features':features,
 							  'labels': labels}
-					
-					basearray['features']['csv'][feature_set]=data
 
-			# only add the label if necessary 
-			label_list=basearray['labels']
-			if labelname not in label_list:
-				label_list.append(labelname)
-			basearray['labels']=label_list
-			transcript_list=basearray['transcripts']
+					print(features)
+					csv_features=basearray['features']['csv']
+					csv_features[feature_set]=data
+					basearray['features']['csv']=csv_features
 
-			# overwrite .JSON 
-			jsonfile=open(listdir[i][0:-4]+'.json','w')
-			json.dump(basearray, jsonfile)
-			jsonfile.close()
+				basearray['labels']=[labelname]
 
-		#except:
-			#print('error')
+				# write to .JSON 
+				jsonfile=open(listdir[i][0:-4]+'.json','w')
+				json.dump(basearray, jsonfile)
+				jsonfile.close()
+
+			elif listdir[i][0:-4]+'.json' in listdir:
+				# load the .JSON file if it is there 
+				basearray=json.load(open(listdir[i][0:-4]+'.json'))
+				transcript_list=basearray['transcripts']
+
+				# only transcribe if you need to (checks within schema)
+				if csv_transcribe==True: 
+					for j in range(len(default_csv_transcriber)):
+						csv_transcriber=default_csv_transcriber[j]
+						if csv_transcriber not in list(transcript_list['csv']):
+							transcript = transcribe_csv(csv_file, csv_transcriber)
+							transcript_list['csv'][csv_transcriber]=transcript 
+							basearray['transcripts']=transcript_list
+						else:
+							transcript = transcript_list['csv'][csv_transcriber]
+
+				# only re-featurize if necessary (checks if relevant feature embedding exists)
+				for j in range(len(feature_sets)):
+					feature_set=feature_sets[j]
+					if feature_set not in list(basearray['features']['csv']):
+						features, labels = cf.csv_featurize(csv_file, cur_dir)
+						print(features)
+
+						try:
+							data={'features':features.tolist(),
+								  'labels': labels}
+						except:
+							data={'features':features,
+								  'labels': labels}
+						
+						basearray['features']['csv'][feature_set]=data
+
+				# only add the label if necessary 
+				label_list=basearray['labels']
+				if labelname not in label_list:
+					label_list.append(labelname)
+				basearray['labels']=label_list
+				transcript_list=basearray['transcripts']
+
+				# overwrite .JSON 
+				jsonfile=open(listdir[i][0:-4]+'.json','w')
+				json.dump(basearray, jsonfile)
+				jsonfile.close()
+
+		except:
+			print('error')

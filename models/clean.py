aa1d4381639b82f084c06c4cffed838fae64cba9
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-05-21 13:58:56 -0400

    Add files via upload

---
+++ models/clean.py
@@ -0,0 +1,13 @@
+import os
+
+listdir=os.listdir()
+
+for i in range(len(listdir)):
+	if listdir[i] not in ['audio_models', 'readme.md'] and listdir[i].endswith('.py') == False:
+
+		if listdir[i].find('.') == -1:
+			shutil.rmtree(listdir[i])
+		else:
+			os.remove(listdir[i])
+
+		print('removed %s'%(listdir[i]))

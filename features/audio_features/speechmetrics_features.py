11ac5d3e02c59ecb9c1fb4ccc75c29636f09fe07
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-06-17 21:10:38 -0400

    Add files via upload

---
+++ features/audio_features/speechmetrics_features.py
@@ -0,0 +1,16 @@
+# the case of absolute metrics
+import speechmetrics
+
+def speechmetrics_featurize(wavfile):
+    window_length = 5 # seconds
+    metrics = speechmetrics.load('absolute', window_length)
+    scores = metrics('test.wav')
+    scores['mosnet'] = float(scores['mosnet'])
+    scores['srmr'] = float(scores['srmr'])
+    features = list(scores.values())
+    labels = list(scores)
+    return features, labels
+
+# features, labels = speechmetrics_featurize('test.wav')
+# print(features)
+# print(labels)

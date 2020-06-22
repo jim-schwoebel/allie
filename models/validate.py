890873eb57d225e89c7427e6746ded2f27e22fd1
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-06-22 16:44:06 -0400

    sample validation script
    for gender detection - can be repurposed to other models in prediction mode.

---
+++ models/validate.py
@@ -0,0 +1,36 @@
+import os,json
+from tqdm import tqdm
+
+def prev_dir(directory):
+	g=directory.split('/')
+	dir_=''
+	for i in range(len(g)):
+		if i != len(g)-1:
+			if i==0:
+				dir_=dir_+g[i]
+			else:
+				dir_=dir_+'/'+g[i]
+	# print(dir_)
+	return dir_
+
+os.chdir(prev_dir(os.getcwd())+'/load_dir/')
+listdir=os.listdir()
+jsonfiles=list()
+
+for i in range(len(listdir)):
+    if listdir[i].endswith('.json'):
+            jsonfiles.append(listdir[i])
+
+predictions=list()
+for i in tqdm(range(len(jsonfiles))):
+	try:
+		g=json.load(open(jsonfiles[i]))
+		models=list(g['models']['audio'])
+		predictions=predictions+models
+	except:
+		print('error')
+
+print('females')
+print(predictions.count('females'))
+print('males')
+print(predictions.count('males'))

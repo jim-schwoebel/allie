20f9a6faa396518b94f62c4eda87939711043880
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-07-12 22:02:52 -0400

    Add files via upload

--- train_dir/delete_features.py
+++ train_dir/delete_features.py
@@ -4,6 +4,7 @@ delete_features.py
 Clear all json files from folders
 '''
 import os, json
+from tqdm import tqdm
 
 folders=list()
 listdir=os.listdir()
@@ -15,7 +16,7 @@ print(folders)
 
 # remove all json files
 curdir=os.getcwd()
-for i in range(len(folders)):
+for i in tqdm(range(len(folders))):
         os.chdir(curdir)
         os.chdir(folders[i])
         listdir=os.listdir()
@@ -25,7 +26,7 @@ for i in range(len(folders)):
                         # print(listdir[i])
                         # os.remove(listdir[i])
                         data=json.load(open(listdir[i]))
-                        del g['features']['audio']['audiotext_features']
+                        del data['features']['audio']['audiotext_features']
                         jsonfile=open(listdir[i],'w')
                         json.dump(data,jsonfile)
                         jsonfile.close()

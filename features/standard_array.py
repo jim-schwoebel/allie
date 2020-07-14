8259279bfbd65c6800542f1998142741158d5e6e
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-07-13 23:51:47 -0400

    Update standard_array.py

--- features/standard_array.py
+++ features/standard_array.py
@@ -4,6 +4,19 @@ This is the standard feature array for Allie (version 1.0).
 Note this will be imported to get back data in all featurization methods
 to ensure maximal code reusability
 '''
+import os 
+
+def prev_dir(directory):
+	g=directory.split('/')
+	dir_=''
+	for i in range(len(g)):
+		if i != len(g)-1:
+			if i==0:
+				dir_=dir_+g[i]
+			else:
+				dir_=dir_+'/'+g[i]
+	# print(dir_)
+	return dir_
 
 def make_features(sampletype):
 
@@ -26,12 +39,18 @@ def make_features(sampletype):
 		'video': dict(),
 		'csv': dict()}
 	
+	# getting settings can be useful to see if settings are the same in every
+	# featurization, as some featurizations can rely on certain settings to be consistent
+	prevdir=prev_dir(os.getcwd())
+	settings=json.load(open('settings.json'))
+	
 	data={'sampletype': sampletype,
 	      'transcripts': transcripts,
 	      'features': features,
 	      'models': models,
 	      'labels': [],
-	      'errors': []
+	      'errors': [],
+	      'settings': settings,
 	     }
 	
 	return data

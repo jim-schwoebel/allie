5d571c8d5db8438a7f147dba220d7af2201338bc
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-07-13 23:44:16 -0400

    Update standard_array.py

--- features/standard_array.py
+++ features/standard_array.py
@@ -9,28 +9,28 @@ def make_features(sampletype):
 
 	# only add labels when we have actual labels.
 	features={'audio':dict(),
-			      'text': dict(),
-			      'image':dict(),
-			      'video':dict(),
-			      'csv': dict()}
+		  'text': dict(),
+		  'image':dict(),
+		  'video':dict(),
+		  'csv': dict()}
 
 	transcripts={'audio': dict(),
-				       'text': dict(),
-				       'image': dict(),
-				       'video': dict(),
-			      	 'csv': dict()}
+		     'text': dict(),
+		     'image': dict(),
+		     'video': dict(),
+		     'csv': dict()}
                
 	models={'audio': dict(),
-		      'text': dict(),
-       	 'image': dict(),
-	     	 'video': dict(),
-    		 'csv': dict()}
+		'text': dict(),
+		'image': dict(),
+		'video': dict(),
+		'csv': dict()}
 	
 	data={'sampletype': sampletype,
-		    'transcripts': transcripts,
-	  	  'features': features,
+	      'transcripts': transcripts,
+	      'features': features,
 	      'models': models,
-	  	  'labels': [],
-        'errors': []}
-        
+	      'labels': [],
+	      'errors': []}
+	
 	return data

84a0777b2c25d301f15d072778ad261879994766
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-07-29 18:04:08 -0400

    Add files via upload

--- cleaning/audio_cleaning/clean_mono16hz.py
+++ cleaning/audio_cleaning/clean_mono16hz.py
@@ -4,6 +4,7 @@ import soundfile as sf
 def clean_mono16hz(audiofile):
 	# replace wavfile with a version that is 16000 Hz mono audio
 	if audiofile.endswith('.wav'):
-		os.system('ffmpeg -i "%s" -ab 16k "%s" -y'%(audiofile,audiofile))
+		os.system('ffmpeg -i "%s" -ar 16000 -ac 1 "%s" -y'%(audiofile,audiofile[0:-4]+'_cleaned.wav'))
+		os.remove(audiofile)
 	elif audiofile.endswith('.mp3'):
-		os.system('ffmpeg -i "%s" -ab 16k "%s" -y'%(audiofile,audiofile[0:-4]+'.wav'))
+		os.system('ffmpeg -i "%s" -ar 16000 -ac 1 "%s" -y'%(audiofile,audiofile[0:-4]+'.wav'))

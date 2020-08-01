92ae3a791533437f286f4970939f348798ca5c31
Jim Schwoebel <jim.schwoebel@gmail.com>
2020-08-01 15:08:37 -0400

    added small bug fix

--- cleaning/csv_cleaning/clean_csv.py
+++ cleaning/csv_cleaning/clean_csv.py
@@ -6,8 +6,9 @@ except:
 	os.system('pip3 install datacleaner==0.1.5')
 
 def clean_csv(csvfile, basedir):
-	# normalize values
-	# more...
+	'''
+	https://github.com/rhiever/datacleaner
+	'''
 	input_dataframe=pd.read_csv(csvfile)
 	newframe=datacleaner.autoclean(input_dataframe, drop_nans=False, copy=False, ignore_update_check=False)
-	newframe.to_csv('clean_'+csvfile, index=0)
+	newframe.to_csv('clean_'+csvfile, index=False)

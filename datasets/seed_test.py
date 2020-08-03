4c7ba9e792187a815eda3b52341fd05da49a91b0
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-03 12:02:44 -0400

    Add files via upload

--- datasets/seed_test.py
+++ datasets/seed_test.py
@@ -16,10 +16,23 @@ def prev_dir(directory):
 	# print(dir_)
 	return dir_
 
-os.system('git clone git@github.com:jim-schwoebel/sample_voice_data.git')
+listdir=os.listdir()
+if 'sample_voice_data' not in listdir:
+	os.system('git clone git@github.com:jim-schwoebel/sample_voice_data.git')
+else:
+	pass
+
 cur_dir=os.getcwd()
 base_dir=prev_dir(cur_dir)
 train_dir=base_dir+'/train_dir'
 
-shutil.copytree(cur_dir+'/sample_voice_data/males',train_dir+'/males')
-shutil.copytree(cur_dir+'/sample_voice_data/females',train_dir+'/females')
+try:
+	shutil.copytree(cur_dir+'/sample_voice_data/males',train_dir+'/males')
+except:
+	shutil.rmtree(train_dir+'/males')
+	shutil.copytree(cur_dir+'/sample_voice_data/males',train_dir+'/males')
+try:
+	shutil.copytree(cur_dir+'/sample_voice_data/females',train_dir+'/females')
+except:
+	shutil.rmtree(train_dir+'/females')
+	shutil.copytree(cur_dir+'/sample_voice_data/females',train_dir+'/females')

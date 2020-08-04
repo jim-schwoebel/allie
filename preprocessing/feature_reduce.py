2953e3125cb7da805ed4291a6923c1b77a0562d1
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-04 19:22:01 -0400

    Update feature_reduce.py

--- preprocessing/feature_reduce.py
+++ preprocessing/feature_reduce.py
@@ -1,7 +1,37 @@
 '''
-Feature selectors:
-- NNI - # https://nni.readthedocs.io/en/latest/FeatureEngineering/GradientFeatureSelector.html
-- Scikit-Learn - 
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
+
+______                                           _             
+| ___ \                                         (_)            
+| |_/ / __ ___ _ __  _ __ ___   ___ ___  ___ ___ _ _ __   __ _ 
+|  __/ '__/ _ \ '_ \| '__/ _ \ / __/ _ \/ __/ __| | '_ \ / _` |
+| |  | | |  __/ |_) | | | (_) | (_|  __/\__ \__ \ | | | | (_| |
+\_|  |_|  \___| .__/|_|  \___/ \___\___||___/___/_|_| |_|\__, |
+              | |                                         __/ |
+              |_|                                        |___/ 
+  ___  ______ _____ 
+ / _ \ | ___ \_   _|
+/ /_\ \| |_/ / | |  
+|  _  ||  __/  | |  
+| | | || |    _| |_ 
+\_| |_/\_|    \___/ 
+
+Employ dimensionality reduction strategies as part of Allie's preprocessing API.
 '''
 
 import json, os, sys

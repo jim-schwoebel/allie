86772fa6e384b1e687f16280cd169c830330e2a9
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-04 19:39:55 -0400

    Update rename.py

--- train_dir/rename.py
+++ train_dir/rename.py
@@ -1,3 +1,36 @@
+'''
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
+
+|_   _|       (_)     (_)              / _ \ | ___ \_   _|
+  | |_ __ __ _ _ _ __  _ _ __   __ _  / /_\ \| |_/ / | |  
+  | | '__/ _` | | '_ \| | '_ \ / _` | |  _  ||  __/  | |  
+  | | | | (_| | | | | | | | | | (_| | | | | || |    _| |_ 
+  \_/_|  \__,_|_|_| |_|_|_| |_|\__, | \_| |_/\_|    \___/ 
+                                __/ |                     
+                               |___/     
+			       
+Renames all the files in a particular directory (both audio files and .JSON files). 
+
+Note you can manually change this to other file types.
+
+Usage: python3 rename_files.py [folder]
+Example: python3 rename.py /Users/jim/desktop/allie/train_dir/males
+'''
 import os,uuid, sys
 
 directory=sys.argv[1]

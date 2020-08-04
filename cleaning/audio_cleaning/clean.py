acc17eb31b179cf85a9b728ff77f0833e13fdc22
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-04 19:03:34 -0400

    Update clean.py

--- cleaning/audio_cleaning/clean.py
+++ cleaning/audio_cleaning/clean.py
@@ -1,11 +1,39 @@
 '''
-Import all the featurization scripts and allow the user to customize what embedding that
-they would like to use for modeling purposes.
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
 
-AudioSet is the only embedding that is a little bit wierd, as it is normalized to the length
-of each audio file. There are many ways around this issue (such as normalizing to the length 
-of each second), however, I included all the original embeddings here in case the time series
-information is useful to you.
+
+/  __ \ |                (_)              / _ \ | ___ \_   _|  _ 
+| /  \/ | ___  __ _ _ __  _ _ __   __ _  / /_\ \| |_/ / | |   (_)
+| |   | |/ _ \/ _` | '_ \| | '_ \ / _` | |  _  ||  __/  | |      
+| \__/\ |  __/ (_| | | | | | | | | (_| | | | | || |    _| |_   _ 
+ \____/_|\___|\__,_|_| |_|_|_| |_|\__, | \_| |_/\_|    \___/  (_)
+                                   __/ |                         
+                                  |___/                          
+  ___            _ _       
+ / _ \          | (_)      
+/ /_\ \_   _  __| |_  ___  
+|  _  | | | |/ _` | |/ _ \ 
+| | | | |_| | (_| | | (_) |
+\_| |_/\__,_|\__,_|_|\___/ 
+                           
+
+This section of Allie's API cleans folders of audio files
+using the default_audio_cleaners.
 '''
 
 ################################################

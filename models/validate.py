ad32f11086360ee77f3234f577b7906c1a319b81
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-04 19:20:49 -0400

    Update validate.py

--- models/validate.py
+++ models/validate.py
@@ -1,3 +1,34 @@
+'''
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
+
+|  \/  |         | |    | |    
+| .  . | ___   __| | ___| |___ 
+| |\/| |/ _ \ / _` |/ _ \ / __|
+| |  | | (_) | (_| |  __/ \__ \
+\_|  |_/\___/ \__,_|\___|_|___/
+
+Can be useful to count the number of model predictions in a certain class after prediction 
+is complete to get an intuition of accuracy of model on new datasets. 
+
+Note you currently have to manually edit this file for it to be useful.
+
+Usage: python3 validate.py
+'''
 import os,json
 from tqdm import tqdm
 import time

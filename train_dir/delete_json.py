d6613d355bbe1dab4b0a66e37d51ee3de1af70df
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-04 19:33:26 -0400

    Update delete_json.py

--- train_dir/delete_json.py
+++ train_dir/delete_json.py
@@ -1,7 +1,28 @@
 '''
-delete_json.py
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
 
-Clear all json files from folders
+|_   _|       (_)     (_)              / _ \ | ___ \_   _|
+  | |_ __ __ _ _ _ __  _ _ __   __ _  / /_\ \| |_/ / | |  
+  | | '__/ _` | | '_ \| | '_ \ / _` | |  _  ||  __/  | |  
+  | | | | (_| | | | | | | | | | (_| | | | | || |    _| |_ 
+  \_/_|  \__,_|_|_| |_|_|_| |_|\__, | \_| |_/\_|    \___/ 
+                                __/ |                     
+                               |___/                      
 '''
 import os
 

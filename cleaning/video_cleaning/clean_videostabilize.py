fdd0a3f75fdb8dc67c29c07f4880e0d66aa15933
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-04 22:09:12 -0400

    Update clean_videostabilize.py

--- cleaning/video_cleaning/clean_videostabilize.py
+++ cleaning/video_cleaning/clean_videostabilize.py
@@ -1,3 +1,43 @@
+'''
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
+
+
+ _____ _                  _                ___  ______ _____     
+/  __ \ |                (_)              / _ \ | ___ \_   _|  _ 
+| /  \/ | ___  __ _ _ __  _ _ __   __ _  / /_\ \| |_/ / | |   (_)
+| |   | |/ _ \/ _` | '_ \| | '_ \ / _` | |  _  ||  __/  | |      
+| \__/\ |  __/ (_| | | | | | | | | (_| | | | | || |    _| |_   _ 
+ \____/_|\___|\__,_|_| |_|_|_| |_|\__, | \_| |_/\_|    \___/  (_)
+                                   __/ |                         
+                                  |___/                          
+ _   _ _     _            
+| | | (_)   | |           
+| | | |_  __| | ___  ___  
+| | | | |/ _` |/ _ \/ _ \ 
+\ \_/ / | (_| |  __/ (_) |
+ \___/|_|\__,_|\___|\___/ 
+
+This script takes a folder of videos (as .mp4s) and converts them
+to stabilized .mp4 videos. This is useful if the videos are wobbly 
+for focusing on video features.
+
+This is enabled if default_video_features = ['clean_videostabilize']
+'''
 # import required libraries
 try:
     from vidgear.gears import VideoGear

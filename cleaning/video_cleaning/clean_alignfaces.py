441740aca30bdd48f907b1b76bb2caf4c1f5ac90
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-04 22:08:03 -0400

    Update clean_alignfaces.py

--- cleaning/video_cleaning/clean_alignfaces.py
+++ cleaning/video_cleaning/clean_alignfaces.py
@@ -1,3 +1,43 @@
+'''
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
+
+
+ _____ _                  _                ___  ______ _____     
+/  __ \ |                (_)              / _ \ | ___ \_   _|  _ 
+| /  \/ | ___  __ _ _ __  _ _ __   __ _  / /_\ \| |_/ / | |   (_)
+| |   | |/ _ \/ _` | '_ \| | '_ \ / _` | |  _  ||  __/  | |      
+| \__/\ |  __/ (_| | | | | | | | | (_| | | | | || |    _| |_   _ 
+ \____/_|\___|\__,_|_| |_|_|_| |_|\__, | \_| |_/\_|    \___/  (_)
+                                   __/ |                         
+                                  |___/                          
+ _   _ _     _            
+| | | (_)   | |           
+| | | |_  __| | ___  ___  
+| | | | |/ _` |/ _ \/ _ \ 
+\ \_/ / | (_| |  __/ (_) |
+ \___/|_|\__,_|\___|\___/ 
+
+This script takes a folder of .mp4 video files and converts 
+them to a folder of .png files full of faces (centered and aligned).
+The original video files are also placed in these folders.
+
+This is enabled if default_video_cleaners=['clean_alignfaces']
+'''
 # you only use these modules if you register, so put them here
 import cv2, os, time, shutil, math
 import skvideo.io, skvideo.motion, skvideo.measure

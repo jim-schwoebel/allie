a8239aba9a7bced6ad853f80e4589d21bd1d33d4
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-04 20:14:25 -0400

    Update train_autopytorch.py

--- training/train_autopytorch.py
+++ training/train_autopytorch.py
@@ -1,3 +1,31 @@
+'''
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
+                                                                             
+|  \/  |         | |    | |  / _ \ | ___ \_   _|
+| .  . | ___   __| | ___| | / /_\ \| |_/ / | |  
+| |\/| |/ _ \ / _` |/ _ \ | |  _  ||  __/  | |  
+| |  | | (_) | (_| |  __/ | | | | || |    _| |_ 
+\_|  |_/\___/ \__,_|\___|_| \_| |_/\_|    \___/ 
+
+Train models using autopytorch: https://github.com/automl/Auto-PyTorch
+
+This is enabled if the default_training_script = ['autopytorch']
+'''
 import os, json, shutil, pickle, sys
 os.system('pip3 install torch==1.5.0')
 import torch

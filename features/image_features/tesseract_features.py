505f9a3d82b3936217942253bc1a8b9e22584b6a
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-05 08:54:22 -0400

    Update tesseract_features.py

--- features/image_features/tesseract_features.py
+++ features/image_features/tesseract_features.py
@@ -1,3 +1,44 @@
+'''
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
+
+______         _                          ___  ______ _____     
+|  ___|       | |                        / _ \ | ___ \_   _|  _ 
+| |_ ___  __ _| |_ _   _ _ __ ___  ___  / /_\ \| |_/ / | |   (_)
+|  _/ _ \/ _` | __| | | | '__/ _ \/ __| |  _  ||  __/  | |      
+| ||  __/ (_| | |_| |_| | | |  __/\__ \ | | | || |    _| |_   _ 
+\_| \___|\__,_|\__|\__,_|_|  \___||___/ \_| |_/\_|    \___/  (_)
+                                                                
+                                                                
+ _____                           
+|_   _|                          
+  | | _ __ ___   __ _  __ _  ___ 
+  | || '_ ` _ \ / _` |/ _` |/ _ \
+ _| || | | | | | (_| | (_| |  __/
+ \___/_| |_| |_|\__,_|\__, |\___|
+                       __/ |     
+                      |___/      
+                      
+Extracts image features if default_image_features = ['tessearct_features']
+
+Transcribes image files with OCR (pytesseract module) and then featurizes
+these transcripts with nltk_features. Read more about nltk_features @
+https://github.com/jim-schwoebel/allie/blob/master/features/text_features/nltk_features.py
+'''
 import os, sys
 from PIL import Image
 import pytesseract 

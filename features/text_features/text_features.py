aed27c9e352389e3f85225ffea88ccc5d3a7d351
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-05 09:02:53 -0400

    Update text_features.py

--- features/text_features/text_features.py
+++ features/text_features/text_features.py
@@ -1,3 +1,44 @@
+'''
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
+
+______         _                          ___  ______ _____     
+|  ___|       | |                        / _ \ | ___ \_   _|  _ 
+| |_ ___  __ _| |_ _   _ _ __ ___  ___  / /_\ \| |_/ / | |   (_)
+|  _/ _ \/ _` | __| | | | '__/ _ \/ __| |  _  ||  __/  | |      
+| ||  __/ (_| | |_| |_| | | |  __/\__ \ | | | || |    _| |_   _ 
+\_| \___|\__,_|\__|\__,_|_|  \___||___/ \_| |_/\_|    \___/  (_)
+                                                                
+                                                                
+ _____         _   
+|_   _|       | |  
+  | | _____  _| |_ 
+  | |/ _ \ \/ / __|
+  | |  __/>  <| |_ 
+  \_/\___/_/\_\\__|
+                   
+		   
+Featurize folders of text files if default_text_features = ['text_features']
+
+This extracts many linguistic features such as the filler ratio,
+type_token_ratio, entropy, standardized_word_entropy,
+question_ratio, number_ratio, brunet's index, honore's statistic,
+and many others.
+'''
 from nltk.tokenize import RegexpTokenizer, word_tokenize
 from nltk.tokenize import word_tokenize
 from nltk import FreqDist

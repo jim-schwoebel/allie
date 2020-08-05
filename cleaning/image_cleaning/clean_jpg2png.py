3161337b173917ad297beba26cb87f21d9792a5f
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-04 22:04:05 -0400

    Update clean_jpg2png.py

--- cleaning/image_cleaning/clean_jpg2png.py
+++ cleaning/image_cleaning/clean_jpg2png.py
@@ -1,3 +1,41 @@
+'''
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
+
+/  __ \ |                (_)              / _ \ | ___ \_   _|  _ 
+| /  \/ | ___  __ _ _ __  _ _ __   __ _  / /_\ \| |_/ / | |   (_)
+| |   | |/ _ \/ _` | '_ \| | '_ \ / _` | |  _  ||  __/  | |      
+| \__/\ |  __/ (_| | | | | | | | | (_| | | | | || |    _| |_   _ 
+ \____/_|\___|\__,_|_| |_|_|_| |_|\__, | \_| |_/\_|    \___/  (_)
+                                   __/ |                         
+                                  |___/                          
+ _____                           
+|_   _|                          
+  | | _ __ ___   __ _  __ _  ___ 
+  | || '_ ` _ \ / _` |/ _` |/ _ \
+ _| || | | | | | (_| | (_| |  __/
+ \___/_| |_| |_|\__,_|\__, |\___|
+                       __/ |     
+                      |___/      
+
+This script takes in a folder of images and simply converts all .jpg files to .pngs.
+
+This is enabled if default_image_cleaners=['clean_jpg2png']
+'''
 from PIL import Image
 import os
 

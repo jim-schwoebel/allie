812aef9b922d9f05d728077dfbea8b373d282bbf
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-04 21:51:52 -0400

    Update clean_random20secsplice.py

--- cleaning/audio_cleaning/clean_random20secsplice.py
+++ cleaning/audio_cleaning/clean_random20secsplice.py
@@ -1,14 +1,54 @@
-  
+'''
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
+
+
+/  __ \ |                (_)              / _ \ | ___ \_   _|  _ 
+| /  \/ | ___  __ _ _ __  _ _ __   __ _  / /_\ \| |_/ / | |   (_)
+| |   | |/ _ \/ _` | '_ \| | '_ \ / _` | |  _  ||  __/  | |      
+| \__/\ |  __/ (_| | | | | | | | | (_| | | | | || |    _| |_   _ 
+ \____/_|\___|\__,_|_| |_|_|_| |_|\__, | \_| |_/\_|    \___/  (_)
+                                   __/ |                         
+                                  |___/                          
+  ___            _ _       
+ / _ \          | (_)      
+/ /_\ \_   _  __| |_  ___  
+|  _  | | | |/ _` | |/ _ \ 
+| | | | |_| | (_| | | (_) |
+\_| |_/\__,_|\__,_|_|\___/ 
+                           
+
+This cleaning script converts all audio files in a given folder to 20 seconds in length.
+
+Note you can change this to any arbitary length by changing the desiredlength function below.
+
+This cleaning script is enabled if default_audio_cleaners=['clean_random20secsplice'] 
+'''
 import soundfile as sf 
 import os, ffmpy, random, getpass
 
 def clean_random20secsplice(audiofile):
+    desiredlength=20
     try:
         data, samplerate = sf.read(audiofile)
         totalframes=len(data)
         totalseconds=int(totalframes/samplerate)
-        startsec=random.randint(0,totalseconds-21)
-        endsec=startsec+20
+        startsec=random.randint(0,totalseconds-(desiredlength+1))
+        endsec=startsec+desiredlength
         startframe=samplerate*startsec
         endframe=samplerate*endsec
         

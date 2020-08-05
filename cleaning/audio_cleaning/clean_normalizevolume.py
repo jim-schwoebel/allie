eebefd0f6427fcf6c60929d5fc472442bd06e903
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-04 21:49:05 -0400

    Update clean_normalizevolume.py

--- cleaning/audio_cleaning/clean_normalizevolume.py
+++ cleaning/audio_cleaning/clean_normalizevolume.py
@@ -1,7 +1,44 @@
+'''
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
+
+
+/  __ \ |                (_)              / _ \ | ___ \_   _|  _ 
+| /  \/ | ___  __ _ _ __  _ _ __   __ _  / /_\ \| |_/ / | |   (_)
+| |   | |/ _ \/ _` | '_ \| | '_ \ / _` | |  _  ||  __/  | |      
+| \__/\ |  __/ (_| | | | | | | | | (_| | | | | || |    _| |_   _ 
+ \____/_|\___|\__,_|_| |_|_|_| |_|\__, | \_| |_/\_|    \___/  (_)
+                                   __/ |                         
+                                  |___/                          
+  ___            _ _       
+ / _ \          | (_)      
+/ /_\ \_   _  __| |_  ___  
+|  _  | | | |/ _` | |/ _ \ 
+| | | | |_| | (_| | | (_) |
+\_| |_/\__,_|\__,_|_|\___/ 
+                           
+
+This cleaning script normalizes the volume using peak normalization and ffmpeg-normalize
+for all audio files in a folder. Note this method could be bad in high-noise environment,
+as noise will be amplified if the noise has high volume.
+
+This cleaning script is enabled if default_audio_cleaners=['clean_normalizevolume'] 
+'''
 import sys, os, ffmpeg_normalize 
-'''
-go to the proper folder from cmd line
-'''
 
 def clean_normalizevolume(audiofile):
     # using peak normalization method 

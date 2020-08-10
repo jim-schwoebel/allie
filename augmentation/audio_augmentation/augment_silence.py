d5d1e7b820bde5f30b82ac4049d3f0a23019cce8
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-10 15:16:52 -0400

    Update augment_silence.py

--- augmentation/audio_augmentation/augment_silence.py
+++ augmentation/audio_augmentation/augment_silence.py
@@ -39,4 +39,4 @@ def augment_silence(filename):
   new_filename=filename[0:-4]+'_trimmed.wav'
   command='sox %s %s silence -l 1 0.1 1'%(filename, new_filename)+"% -1 2.0 1%"
   os.system(command)
-  return [new_filename]
+  return [filename, new_filename]

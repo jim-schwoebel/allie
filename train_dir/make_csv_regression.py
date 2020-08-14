13ca8b5a67421490ca585cc927175ab7a474946d
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-13 20:55:15 -0400

    Add files via upload

---
+++ train_dir/make_csv_regression.py
@@ -0,0 +1,46 @@
+'''
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
+                                                                             
+|  \/  |         | |    | |  / _ \ | ___ \_   _|
+| .  . | ___   __| | ___| | / /_\ \| |_/ / | |  
+| |\/| |/ _ \ / _` |/ _ \ | |  _  ||  __/  | |  
+| |  | | (_) | (_| |  __/ | | | | || |    _| |_ 
+\_|  |_/\___/ \__,_|\___|_| \_| |_/\_|    \___/ 
+
+Make many CSV files for modeling,
+good to combine with https://github.com/jim-schwoebel/allie/blob/master/training/regression_all.py
+
+'''
+import os
+import pandas as pd
+
+data=pd.read_csv('new.csv')
+cols=list(data)
+
+for i in range(len(cols)-1):
+	ind1=cols.index('url')
+	ind2=cols.index(cols[i+1])
+	delcols=list()
+	for j in range(len(cols)):
+		if j in [ind1,ind2]:
+			pass
+		else:
+			delcols.append(cols[j])
+	print(delcols)
+	newdata=data.drop(delcols, axis=1)
+	newdata.to_csv(cols[i+1]+'.csv', index=False)

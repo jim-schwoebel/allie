c79aa48c1067610e27aac3bf555dba36899872ed
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-18 06:51:29 -0400

    improved documentation

--- train_dir/create_dataset.py
+++ train_dir/create_dataset.py
@@ -1,7 +1,34 @@
 '''
-How to use
-python3 create_dataset.py [csvfile] [targetname]
-python3 create_dataset.py What_is_your_total_household_income.csv 'What is your total household income?'
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
+                                                                             
+|  \/  |         | |    | |  / _ \ | ___ \_   _|
+| .  . | ___   __| | ___| | / /_\ \| |_/ / | |  
+| |\/| |/ _ \ / _` |/ _ \ | |  _  ||  __/  | |  
+| |  | | (_) | (_| |  __/ | | | | || |    _| |_ 
+\_|  |_/\___/ \__,_|\___|_| \_| |_/\_|    \___/ 
+
+Usage: python3 create_dataset.py [csvfile] [targetname]
+
+Example: python3 create_dataset.py What_is_your_total_household_income.csv 'What is your total household income?'
+--> outputs two folders: ['What is your total household income_below','What is your total household income_above']
+
+Note that these two classes are centered around the mean of a regression problem spreadsheet (e.g. to allow for 
+data visualization).
 '''
 import pandas as pd
 import numpy as np

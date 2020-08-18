f4805037d1fad3ef8e7a41bcafc5bc21c14903bf
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-18 09:33:30 -0400

    Update delete_features.py

--- train_dir/delete_features.py
+++ train_dir/delete_features.py
@@ -1,21 +1,21 @@
 '''
-			   AAA               lllllll lllllll   iiii                      
-			  A:::A              l:::::l l:::::l  i::::i                     
-			 A:::::A             l:::::l l:::::l   iiii                      
-			A:::::::A            l:::::l l:::::l                             
-		   A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
-		  A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
-		 A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
-		A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
-	   A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
-	  A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
-	 A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
-	A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
    A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
   A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
  A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
 AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
-																			 
+                                                                             
 |  \/  |         | |    | |  / _ \ | ___ \_   _|
 | .  . | ___   __| | ___| | / /_\ \| |_/ / | |  
 | |\/| |/ _ \ / _` |/ _ \ | |  _  ||  __/  | |  

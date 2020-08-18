16d8524bc7d185fe3d27939da281f5fce0c21ea2
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-18 06:49:00 -0400

    improvements to the script
    Adds CLI functionality

--- train_dir/create_dataset.py
+++ train_dir/create_dataset.py
@@ -1,13 +1,32 @@
+'''
+How to use
+python3 create_dataset.py [csvfile] [targetname]
+python3 create_dataset.py What_is_your_total_household_income.csv 'What is your total household income?'
+'''
 import pandas as pd
-import os, shutil
+import numpy as np
+import os, shutil, sys
 
-data=pd.read_csv('pga1_lupus.csv')
+csvfile=sys.argv[1]
+target=sys.argv[2]
+
+data=pd.read_csv(csvfile)
 urls=(data['url'])
-sleqols=list(data['pga1_lupus_active'])
-os.mkdir('pga1_lupus_active_above5.65')
-os.mkdir('pga1_lupus_active_below5.65')
+data_values=list(data[target])
+os.mkdir(target+'_above')
+os.mkdir(target+'_below')
+average=float(np.average(np.array(data_values)))
+
 for i in range(len(data)):
-	if sleqols[i] > 5.65:
-		shutil.copy(urls[i], os.getcwd()+'/'+'pga1_lupus_active_above5.65'+'/'+urls[i].split('/')[-1])
+	if data_values[i] > average:
+		shutil.copy(urls[i], os.getcwd()+'/'+target+'_above'+'/'+urls[i].split('/')[-1])
+		try:
+			shutil.copy(urls[i][0:-4]+'.json', os.getcwd()+'/'+target+'_above'+'/'+urls[i].split('/')[-1][0:-4]+'.json')
+		except:
+			pass
 	else:
-		shutil.copy(urls[i], os.getcwd()+'/'+'pga1_lupus_active_below5.65'+'/'+urls[i].split('/')[-1])
+		shutil.copy(urls[i], os.getcwd()+'/'+target+'_below'+'/'+urls[i].split('/')[-1])
+		try:
+			shutil.copy(urls[i][0:-4]+'.json', os.getcwd()+'/'+target+'_below'+'/'+urls[i].split('/')[-1][0:-4]+'.json')
+		except:
+			pass

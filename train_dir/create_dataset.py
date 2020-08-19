06ea3939df8ef3abe8f95916c9bd524866a06149
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2020-08-19 12:43:27 -0400

    Update create_dataset.py

--- train_dir/create_dataset.py
+++ train_dir/create_dataset.py
@@ -1,6 +1,32 @@
 '''
-How to use
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
+                                                                             
+|  \/  |         | |    | |  / _ \ | ___ \_   _|
+| .  . | ___   __| | ___| | / /_\ \| |_/ / | |  
+| |\/| |/ _ \ / _` |/ _ \ | |  _  ||  __/  | |  
+| |  | | (_) | (_| |  __/ | | | | || |    _| |_ 
+\_|  |_/\___/ \__,_|\___|_| \_| |_/\_|    \___/ 
+			       
+			       
+How to use:
+
 python3 create_dataset.py [csvfile] [targetname]
+
 python3 create_dataset.py What_is_your_total_household_income.csv 'What is your total household income?'
 '''
 import pandas as pd

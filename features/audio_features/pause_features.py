89189ee215cc8a999da0f5006b9dde89491d81f6
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2021-05-01 15:14:20 -0400

    Update pause_features.py

--- features/audio_features/pause_features.py
+++ features/audio_features/pause_features.py
@@ -79,8 +79,8 @@ def pause_featurize(wavfile):
     pauses=list()
     pause_lengths=list()
     for i in range(len(utterances)-1):
-        pauses.append([utteraces[i+1][0], utterances[i][1]])
-        pause_length=utteraces[i+1][0] - utterances[i][1]
+        pauses.append([utterances[i+1][0], utterances[i][1]])
+        pause_length=utterances[i+1][0] - utterances[i][1]
         pause_lengths.append(pause_length)
     
     # get descriptive stats of pause leengths

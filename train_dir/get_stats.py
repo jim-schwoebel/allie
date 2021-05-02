12a2babb8b5012dff2418a363b02b9fb1e612d6e
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2021-05-02 12:14:37 -0400

    Add files via upload

---
+++ train_dir/get_stats.py
@@ -0,0 +1,75 @@
+from docx import Document
+from docx.shared import Cm, Pt
+import numpy as np
+import os, json, time
+
+def describe_text(jsonfile):
+
+	# get dictionary 
+	g=json.load(open(jsonfile))
+	features=g['features']['audio']
+	featuretypes=list(features)
+
+	features_=list()
+	labels_=list()
+	for j in range(len(featuretypes)):
+		features_=features_+features[featuretypes[j]]['features']
+		labels_=labels_+features[featuretypes[j]]['labels']
+
+	description=dict(zip(labels_,features_))
+	
+	return description
+
+def get_descriptive_statistics(dict_, labels_):
+	for j in range(len(labels_)):
+		dict_[labels[j]]=str(np.mean(np.array(dict_[labels[j]])))+' (+/- '+str(np.std(np.array(dict_[labels[j]])))+')'
+	return dict_
+
+listdir=os.listdir()
+jsonfiles=list()
+for i in range(len(listdir)):
+	if listdir[i].endswith('.json'):
+		jsonfiles.append(listdir[i])
+
+# got all the jsonfiles, now add to each feature 
+description=describe_text(jsonfiles[0])
+labels=list(description)
+
+dict_=dict()
+for i in range(len(labels)):
+	dict_[labels[i]]=[]
+
+# now go through all the json files 
+for i in range(len(jsonfiles)):
+	stats=describe_text(jsonfiles[i])
+	print(stats)
+	for j in range(len(labels)):
+		dict_[labels[j]]=dict_[labels[j]]+[stats[labels[j]]]
+
+dict_=get_descriptive_statistics(dict_, labels)
+
+text_stats=dict_
+
+# make the table! (alphabetized)
+text_stats['A_Title']='TITLE'
+text_stats = dict(sorted(text_stats.items()))
+
+# customizing the table
+word_document = Document()
+document_name = 'news-article-stats'
+table = word_document.add_table(0, 0) # we add rows iteratively
+table.style = 'TableGrid'
+first_column_width = 5
+second_column_with = 10
+table.add_column(Cm(first_column_width))
+table.add_column(Cm(second_column_with))
+
+for index, stat_item in enumerate(text_stats.items()):
+	table.add_row()
+	stat_name, stat_result = stat_item
+	row = table.rows[index]
+	row.cells[0].text = str(stat_name)
+	row.cells[1].text = str(stat_result)
+word_document.add_page_break()
+
+word_document.save(document_name + '.docx')

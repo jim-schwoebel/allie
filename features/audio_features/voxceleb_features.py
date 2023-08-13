4fd5118b8cb941a9c94686f47cf8e4bd2a3d7408
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2023-08-12 21:16:35 -0400

    Add files via upload

---
+++ features/audio_features/voxceleb_features.py
@@ -0,0 +1,70 @@
+'''
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
+
+|  ___|       | |                        / _ \ | ___ \_   _|  _ 
+| |_ ___  __ _| |_ _   _ _ __ ___  ___  / /_\ \| |_/ / | |   (_)
+|  _/ _ \/ _` | __| | | | '__/ _ \/ __| |  _  ||  __/  | |      
+| ||  __/ (_| | |_| |_| | | |  __/\__ \ | | | || |    _| |_   _ 
+\_| \___|\__,_|\__|\__,_|_|  \___||___/ \_| |_/\_|    \___/  (_)
+                                                                
+                                                                
+  ___            _ _       
+ / _ \          | (_)      
+/ /_\ \_   _  __| |_  ___  
+|  _  | | | |/ _` | |/ _ \ 
+| | | | |_| | (_| | | (_) |
+\_| |_/\__,_|\__,_|_|\___/ 
+                           
+
+This will featurize folders of audio files if the default_audio_features = ['voxceleb_features']
+
+This helps to detect speaker identities.
+'''
+import os
+import torchaudio
+import numpy as np
+from speechbrain.pretrained import EncoderClassifier
+
+classifier = EncoderClassifier.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb")
+
+def remove_empty_strings(input_list):
+    while '' in input_list:
+        input_list.remove('')
+    return input_list
+
+def detect_speaker(wavfile_1: str, wavfile_2: str):
+    # classifier = EncoderClassifier.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb")
+    # signal, fs =torchaudio.load(wavfile_1)
+    # embeddings = classifier.encode_batch(signal)
+    # print(embeddings)
+    verification = SpeakerRecognition.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb", savedir="pretrained_models/spkrec-ecapa-voxceleb")
+    score, prediction = verification.verify_files(wavfile_1, wavfile_2) 
+    print(score)
+    print(prediction)
+    return score, prediction
+
+def voxceleb_featurize(wavfile, classifier=classifier):
+    
+    signal, fs =torchaudio.load(wavfile)
+    features = classifier.encode_batch(signal).flatten()
+
+    labels=[]
+    for i in range(len(features)):
+        labels.append('voxceleb_'+str(i))
+
+    return features, labels

5fee69d03583fa90f6ae068c8ae605282d645103
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2023-08-20 12:29:10 -0400

    Add files via upload
    fix confusion matrix for many classes.

--- training/model.py
+++ training/model.py
@@ -399,38 +399,39 @@ def plot_roc_curve(y_test, probs, clf_names):
 	plt.savefig('roc_curve.png')
 	plt.close()
 
-def plot_confusion_matrix(cm, classes, normalize=True, title='Confusion matrix', cmap=plt.cm.Blues):
-	"""
-	This function prints and plots the confusion matrix.
-	Normalization can be applied by setting `normalize=True`.
-	"""
-	if normalize:
-		cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-		print("\nNormalized confusion matrix")
-	else:
-		print('\nConfusion matrix, without normalization')
+def plot_confusion_matrix(cm, classes, normalize=True, title='Confusion matrix', cmap=plt.cm.Blues, figsize=(10, 8), threshold=0.5):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("\nNormalized confusion matrix")
+    else:
+        print('\nConfusion matrix, without normalization')
 
-	plt.imshow(cm, interpolation='nearest', cmap=cmap)
-	plt.title(title)
-	plt.colorbar()
-	tick_marks = np.arange(len(classes))
-	plt.xticks(tick_marks, classes, rotation=45)
-	plt.yticks(tick_marks, classes)
+    plt.figure(figsize=figsize)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45, ha='right')
+    plt.yticks(tick_marks, classes)
 
-	fmt = '.2f' if normalize else 'd'
-	thresh = cm.max() / 2.
-	for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-		plt.text(j, i, format(cm[i, j], fmt),
-				 horizontalalignment="center",
-				 color="white" if cm[i, j] > thresh else "black")
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() * threshold
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if cm[i, j] > 0.001:  # You can adjust this threshold as needed
+            plt.text(j, i, format(cm[i, j], fmt),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
 
-	plt.tight_layout()
-	plt.ylabel('True label')
-	plt.xlabel('Predicted label')
-	plt.tight_layout()
-	plt.savefig('confusion_matrix.png')
-	plt.close()
-
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+    plt.savefig('confusion_matrix.png')
+    plt.close()
 def plot_regressor(regressor, classes, X_test, y_test):
 	'''
 	plot regression models with a bar chart.
